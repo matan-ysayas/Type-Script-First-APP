8d0e901a6f3718ff01b2a617274d4c9bba901ccc
matan-ysayas <matan.ysayas@gmail.com>
2022-02-17 09:10:55 +0200

    fix

--- src/component/Game/Game.tsx
+++ src/component/Game/Game.tsx
@@ -1,38 +1,60 @@
 const Game = () => {
-  const NumberCheck = (userNum:number) => {
-  
+  const NumberCheck = (userNum: number) => {
     let higshNum: number = userNum;
     let lowNum: number = userNum;
     while (userNum !== 0) {
-         userNum = Number(prompt("enter number"));
-        if (userNum > higshNum) {
-            higshNum = userNum;
-          }  if (userNum < lowNum) {
-            lowNum = userNum;
-          }
+      userNum = Number(prompt("enter number"));
+      if (userNum > higshNum) {
+        higshNum = userNum;
+      }
+      if (userNum < lowNum) {
+        lowNum = userNum;
+      }
     }
     console.log(`the highes num is${higshNum} and the lowe num is${lowNum}`);
-
   };
-  const Get10=(userNum:number)=>{
-      let courtier=0
-      let numArray:number[]=[]
-      while(courtier<10){
-    
-      userNum=Number(prompt("enter number"));
-      numArray.push(userNum)
+  const Get10 = (userNum: number) => {
+    let courtier = 0;
+    let numArray: number[] = [];
+    while (courtier < 10) {
+      userNum = Number(prompt("enter number"));
+      numArray.push(userNum);
       courtier++;
-      }
-      console.log(numArray.filter((item)=>item%2==0));
-      
+    }
+    console.log(numArray.filter((item) => item % 2 == 0));
+  };
+  const Array100 = (num: number) => {
+    let counter = 0;
+    let randomArray: number[] = [];
+    while (counter < 100) {
+      randomArray.push(Math.floor(Math.random() * 100));
+      counter++;
+    }
+    if (randomArray.find((item) => item == num)) {
+      console.log(randomArray);
 
-  }
+      console.log("number is in the array");
+    } else {
+      console.log(randomArray);
+      console.log("number is not in the array");
+    }
+  };
 
-  return <div>
-    <button onClick={()=>NumberCheck(Number(prompt("enter number")))}> 21</button>
-    <button onClick={()=>Get10(Number(prompt("enter number")))}>22</button>
-  
+  return (
+    <div>
+      <button onClick={() => NumberCheck(Number(prompt("enter number")))}>
+        21
+      </button>
 
-  </div>;
+      <button onClick={() => Get10(Number(prompt("enter number")))}>22</button>
+      <button
+        onClick={() => {
+          Array100(10);
+        }}
+      >
+        23
+      </button>
+    </div>
+  );
 };
 export default Game;
